8a5f54c84e4f8e7c5d8bb5e035258f306c7036aa
Bryant Oblad <bryant.oblad@gmail.com>
2016-10-24 21:36:30 -0600

    finishing assignment 7

--- Assignment7/analyze_lums_and_temps.py
+++ Assignment7/analyze_lums_and_temps.py
@@ -11,8 +11,8 @@ ap.add_argument('-tf', '--temp_file', required = True, help = 'Path to temp file
 args = vars(ap.parse_args())
 
 ## define regexes
-lum_entry_pat  = r''
-temp_entry_pat = r''
+lum_entry_pat  = r"^.*\_(.*)\-.*\s+(.*)"
+temp_entry_pat = r"^.*\_(.*)\-.*\s+(.*)"
 
 ## define two dictionaries
 lum_tbl = {}
@@ -20,34 +20,39 @@ tmp_tbl = {}
 
 with open(args['lum_file']) as infile:
     for line in infile:
-        m = re.match(lum_entry_pat, line)
+        m = re.match(lum_entry_pat, line.replace('\n', ''))
         if m != None:
-            ## your code to fill up lum_tbl
+            if m.group(1)[:2] in lum_tbl:
+                lum_tbl[m.group(1)[:2]].append(float(m.group(2)))
+            else:
+                lum_tbl[m.group(1)[:2]] = [float(m.group(2))]
 
 with open(args['temp_file']) as infile:
     for line in infile:
-        print line
-        m = re.match(temp_entry_pat, line)
+        m = re.match(temp_entry_pat, line.replace('\n', ''))
         if m != None:
-            ## your code to fill up tmp_tbl
+            if m.group(1)[:2] in tmp_tbl:
+                tmp_tbl[m.group(1)[:2]].append(float(m.group(2)))
+            else:
+                tmp_tbl[m.group(1)[:2]] = [float(m.group(2))]
 
 ## print tables and averages
 print('Luminosity Table')
 for h, lums in lum_tbl.items():
-    print h, '-->', str(lums)
-print
+    print(h, '-->', str(lums))
+print(' ')
 
 print('Temperature Table')
 for h, temps in tmp_tbl.items():
-    print h, '-->', str(temps)
-print
+    print(h, '-->', str(temps))
+print(' ')
 
 print('Luminosity Averages')
 for h, lums in lum_tbl.items():
-    print h, '-->', sum(lums)/len(lums)
-print
+    print(h, '-->', sum(lums)/len(lums))
+print(' ')
 
 print('Temperature Averages')
 for h, temps in tmp_tbl.items():
-    print h, '-->', sum(temps)/len(temps)
-print    
+    print(h, '-->', sum(temps)/len(temps))
+print(' ')
--- Assignment7/gen_avrg_lum.py
+++ Assignment7/gen_avrg_lum.py
@@ -4,6 +4,7 @@ import sys
 import os
 import fnmatch
 import re
+from PIL import Image
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-id', '--idir', required = True, help = 'Path to image')
@@ -14,18 +15,25 @@ def display_image_pix(image, h, w):
     image_pix = list(image)
     for r in xrange(h):
         for c in xrange(w):
-            print list(image_pix[r][c]), ' ',
-        print
+            print(list(image_pix[r][c]), ' ')
+        print(' ')
 
 ## luminosity conversion
 def luminosity(rgb, rcoeff=0.2126, gcoeff=0.7152, bcoeff=0.0722):
     return rcoeff*rgb[0]+gcoeff*rgb[1]+bcoeff*rgb[2]
 
 def compute_avrg_luminosity(imagepath):
-    import ipdb; ipdb.set_trace()
+    image = Image.open(imagepath)
+    total = 0.0
+    for x in range(image.width):
+        for y in range(image.height):
+            total = total + luminosity(image.getpixel((x, y)))
+    return total / (image.width * image.height)
 
 def gen_avrg_lumin_for_dir(imgdir, filepat):
-    yield compute_avrg_luminosity('{imgdir}{filepat}'.format)
+    for path, dirlist, filelist in os.walk(imgdir):
+        for file_name in fnmatch.filter(filelist, filepat):
+            yield (file_name, compute_avrg_luminosity(os.path.join(path, file_name)))
 
 ## run ghe generator and output into STDOUT
 for fp, lum_avrg in gen_avrg_lumin_for_dir(args['idir'], r'*.png'):
