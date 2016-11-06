b20fc35c4d9387fdcc42567b87ea2fffe2ce13e3
boblad <bryant.oblad@gmail.com>
2016-11-06 02:41:08 +0000

    giving up

--- Assignment8/ht_detect_lanes.py
+++ Assignment8/ht_detect_lanes.py
@@ -7,7 +7,10 @@ import os
 import fnmatch
 
 def line_deg_angle(x1, y1, x2, y2):
-    pass
+    y = y2 - y1
+    x = x2 - x1
+    slope = y / x
+    return (numpy.arctan(slope) * 180) / math.pi
 
 ## >>> line_deg_angle(1, 1, 5, 5)
 ##45.0
@@ -15,7 +18,7 @@ def line_deg_angle(x1, y1, x2, y2):
 ##-45.0
 
 def unit_test_01(x1, y1, x2, y2):
-    print line_deg_angle(x1, y1, x2, y2)
+    print(line_deg_angle(x1, y1, x2, y2))
 
 def draw_ht_lines_in_image(image, lines, color, line_thickness):
     try:
@@ -23,39 +26,43 @@ def draw_ht_lines_in_image(image, lines, color, line_thickness):
             x1, y1, x2, y2 = ln[0]
             cv2.line(image, (x1, y1), (x2, y2), color, line_thickness)
     except Exception as e:
-        print str(e)
+        print(str(e))
 
-def display_lines_and_angles(lines):
+def display_line_angles(lines):
     try:
         for ln in lines:
             x1, y1, x2, y2 = ln
-            print (x1, y1, x2, y2), line_deg_angle(x1, y1, x2, y2)
+            print( (x1, y1, x2, y2), line_deg_angle(x1, y1, x2, y2))
     except Exception as e:
-        print str(e)
+        print(str(e))
 
 def display_ht_lines_and_angles(lines):
     try:
         for ln in lines:
             x1, y1, x2, y2 = ln[0]
-            print (x1, y1, x2, y2), line_deg_angle(x1, y1, x2, y2)
+            print((x1, y1, x2, y2), line_deg_angle(x1, y1, x2, y2))
     except Exception as e:
-        print str(e)
+        print(str(e))
 
 def is_angle_in_range(a, ang_lower, ang_upper):
-    pass
+   if a <= ang_lower and a >= ang_upper:
+       return true
+   return false
 
 def is_left_lane_line(x1, y1, x2, y2, ang_lower, ang_upper):
-    pass
+   angle = line_deg_angle(x1, y1, x2, y2)
+   return is_angle_in_range(angle, ang_lower, ang_upper)
 
 def is_right_lane_line(x1, y1, x2, y2, ang_lower, ang_upper):
-    pass
+    angle = line_deg_angle(x1, y1, x1, x2)
+    return is_angle_in_range(angle, ang_lower, ang_upper)
 
 def display_ht_lanes_in_image(image_path, rho_accuracy, theta_accuracy, num_votes, min_len, max_gap):
     image = cv2.imread(image_path) ## read the image
     gray  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) ## grayscale
     edges = cv2.Canny(gray, 50, 150, apertureSize=3) ## detect edges
     lines = cv2.HoughLinesP(edges, rho_accuracy, theta_accuracy, num_votes, min_len, max_gap) ## detect hough lines
-    print 'Detected lines and angles:'
+    print('Detected lines and angles:')
     display_ht_lines_and_angles(lines)
     draw_ht_lines_in_image(image, lines, (255, 0, 0), 2)
     cv2.imshow('Gray',  gray)
@@ -72,6 +79,15 @@ def filter_left_lane_lines(lines, ang_lower=-60, ang_upper=-30):
 def filter_right_lane_lines(lines, ang_lower=30, ang_upper=60):
     pass
 
+def draw_lines_in_image(image, lines, color, line_thickness):
+    try:
+        for ln in lines:
+            x1, y1, x2, y2 = ln
+            cv2.line(image, (x1, y1), (x2, y2), color, line_thickness)
+    except Exception as e:
+        print(str(e))
+
+
 ## most common value for rho_accuracy is 1
 ## most common value for theta_accuracy is numpy.pi/180.
 def plot_ht_lanes_in_image(image_path, rho_accuracy, theta_accuracy, num_votes, min_len, max_gap):
@@ -84,10 +100,10 @@ def plot_ht_lanes_in_image(image_path, rho_accuracy, theta_accuracy, num_votes, 
     cv2.imshow('Edges', edges)
     ll_lines = filter_left_lane_lines(lines)
     rl_lines = filter_right_lane_lines(lines)
-    draw_lines_in_image(image, ll_lines, (255, 0, 0))
-    draw_lines_in_image(image, rl_lines, (0, 0, 255))
-    print 'detected', len(ll_lines), 'left lanes'
-    print 'detected', len(rl_lines), 'right lanes'
+    draw_lines_in_image(image, ll_lines, (255, 0, 0), 10)
+    draw_lines_in_image(image, rl_lines, (0, 0, 255), 10)
+    print('detected', len(ll_lines), 'left lanes')
+    print('detected', len(rl_lines), 'right lanes')
     cv2.imshow('Lanes in Image', image)
     cv2.waitKey(0)
 
@@ -105,8 +121,8 @@ def detect_ht_lanes_in_image(image_path, rho_accuracy, theta_accuracy, num_votes
 
 def unit_test_03(image_path, rho_accuracy, theta_accuracy, num_votes, min_len, max_gap):
     ll, rl = detect_ht_lanes_in_image(image_path, rho_accuracy, theta_accuracy, num_votes, min_len, max_gap)
-    print 'number of left lanes:', ll
-    print 'number of right lanes:', rl
+    print('number of left lanes:', ll)
+    print('number of right lanes:', rl)
     
 def find_ll_rl_lanes_in_images_in_dir(imgdir, filepat, rho_acc, th_acc, num_votes, min_len, max_gap):
     pass
@@ -114,13 +130,15 @@ def find_ll_rl_lanes_in_images_in_dir(imgdir, filepat, rho_acc, th_acc, num_vote
 def unit_test_04(imgdir, filepat, rho_acc, th_acc, num_votes, min_len, max_gap):
     for fp, ll_rl in find_ll_rl_lanes_in_images_in_dir(imgdir, filepat, 1, numpy.pi/180,
                                          num_votes, min_len, max_gap):
-       print fp, ll_rl[0], ll_rl[1]
+       print(fp, ll_rl[0], ll_rl[1])
     
 if __name__ == '__main__':
-    #unit_test_01(sys.argv[1], 1, numpy.pi/180, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
-    #plot_ht_lanes_in_image(sys.argv[1], 1, numpy.pi/180, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+   # unit_test_01(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+    print(sys.argv[1])
+    print(sys.argv[2])    
+    plot_ht_lanes_in_image(sys.argv[1], 1, numpy.pi/180, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
     #unit_test_02(sys.argv[1], 1, numpy.pi/180, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
-    #unit_test_03(sys.argv[1], sys.argv[2], 1, numpy.pi/180, int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
+    #unit_test_03(sys.argv[1], sys.argv[2], 1, numpy.pi/180, int(sys.argv[3]), int(sys.argv[4]))
     #unit_test_04(sys.argv[1], sys.argv[2], 1, numpy.pi/180, int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
     pass
 
